68848fcd716172c4cb3d1ab6a0846b6c04326531
vasiliy92 <vasiliy.krasov.job@gmail.com>
2026-05-17 00:41:37 +0400

    rm context.ts

--- frontend/src/app/admin/context.ts
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext } from 'react';
-
-export type Locale = 'en' | 'fr' | 'ru';
-
-export const LocaleContext = createContext<Locale>('en');
-export const MessagesContext = createContext<any>(null);
-
-export function useLocale() { return useContext(LocaleContext); }
-export function useMessages() { return useContext(MessagesContext); }
